b9cf1fde4f0439a0999c1bc3fc801783053832fc
edwardgunning <edward.gunning@ul.ie>
2023-02-26 20:31:24 +0000

    more work on basis expansion plots

--- code/analysis/chapter-02/basis-functions-plot.R
+++ code/analysis/chapter-02/basis-functions-plot.R
@@ -20,40 +20,12 @@ theme_update(axis.text = element_text(size = 10),
              strip.text = element_text(size = 12))
 
 
-
-# 3) Data  ----------------------------------------------------------
-
-# use the gait data set:
-data('gait')
-dimnames(gait)
-dim(gait)
-# 20 x 29 x 2 array
-# rows are time points
+# -------------------------------------------------------------------------
 gait_time <- as.numeric(dimnames(gait)[[1]])
-# choose a single observation:
-gait_trial <- gait[, 1 , 2]
-
-# 4) Set up bases ---------------------------------------------------------
-# 9 (nbasis = 9) cubic (norder = 4) bspline basis functions 
-# defined on the interval of the gait data 
-# with equally spaced knots
 bspline_9 <- create.bspline.basis(rangeval = range(gait_time), nbasis = 9, norder = 4)
-plot(bspline_9, knots = F, lty = 1)
 
 # 9 Fourier basis functions:
 fourier_9 <- create.fourier.basis(rangeval = range(gait_time), nbasis = 9)
-plot(fourier_9, lty = 1)
-
-
-# 5) Smoothing ------------------------------------------------------------
-
-# smooth on bspline basis (least-squares)
-smooth_bs <- smooth.basis(argvals = gait_time, y = gait_trial, fdParobj = bspline_9)
-smooth_f <- smooth.basis(argvals = gait_time, y = gait_trial, fdParobj = fourier_9)
-
-
-
-# 6) Evaluate Basis and Data Wrangling --------------------------------------
 
 # sequence of time points to evaluate data at 
 evaluation_sequence <- seq(min(gait_time), max(gait_time), length.out = 501)
@@ -62,23 +34,23 @@ evaluation_sequence <- seq(min(gait_time), max(gait_time), length.out = 501)
 bspl_eval <- eval.basis(evalarg = evaluation_sequence, basisobj = bspline_9)
 fourier_eval <- eval.basis(evalarg = evaluation_sequence, basisobj = fourier_9)
 
-# weight them according to their coefficients (from smoothing)
-bs_weighted <- bspl_eval %*%  diag(as.vector(smooth_bs$fd$coefs)) %>% as.data.frame
-f_weighted <- fourier_eval %*%  diag(as.vector(smooth_f$fd$coefs)) %>% as.data.frame
+bs_eval_df<- bspl_eval %>% as.data.frame
+f_eval_df<- fourier_eval %>% as.data.frame
 
-# name columns of matrices
-colnames(bs_weighted) <- colnames(f_weighted) <- paste0("fun_", 1:9)
+colnames(bs_eval_df) <- colnames(f_eval_df) <- paste0("fun_", 1:9)
+
+
 
 # calculate the sum of all weighted basis functions
 # this is just the fitted function in each case (i.e. the weighted sum)
-bs_weighted$total <- rowSums(bs_weighted)
-f_weighted$total <- rowSums(f_weighted)
+bs_eval_df$total <- rowSums(bs_eval_df)
+f_eval_df$total <- rowSums(f_eval_df)
 
 # data format: wide to long
-bs_lng <- data.frame(time = evaluation_sequence, bs_weighted) %>%
+bs_lng <- data.frame(time = evaluation_sequence, bs_eval_df) %>%
   gather(-time, key = "fun", value = "val_bs")
 
-f_lng <- data.frame(time = evaluation_sequence, f_weighted) %>%
+f_lng <- data.frame(time = evaluation_sequence, f_eval_df) %>%
   gather(-time, key = "fun", value = "val_f")
 
 # and combine:
@@ -87,8 +59,7 @@ basis_df <- inner_join(bs_lng, f_lng, by = c("time", "fun")) %>%
   mutate(basis_type = case_when(basis_type == "val_bs" ~ "B-spline",
                                 basis_type == "val_f" ~ "Fourier"))
 
-# and get data frame of original data to plot points
-original_data <- data.frame(time = gait_time, val = gait_trial)
+
 
 # 7) Plot --------------------------------------------------------------------
 
@@ -100,16 +71,15 @@ my_pallette_39 <- createPalette(N = 39, seedcolors = c("#ff0000", "#00ff00", "#0
 p3 <- basis_df %>%
   ggplot() +
   aes(x = time, y = val) +
-  facet_wrap( ~ basis_type) +
+  facet_wrap( ~ basis_type, scales ="free_y") +
   geom_line(mapping = aes(group = fun, color = fun),
             data = . %>% filter(!(fun %in% c("total")))) +
-  geom_line(data = . %>% filter((fun %in% c("total"))), linewidth = 0.8) +
-  geom_point(data = original_data) + 
   scale_x_continuous(expand = c(0, 0)) + 
   scale_color_manual(values = my_pallette_39) +
   labs(x = "Normalised Time (Proportion of Gait Cycle)",
        y = "Angle ($^{\\circ}$)") + # axis labels
   theme(legend.position = "none") # don't need a legend
+
 
 # View plot
 # Save Plots 
@@ -117,10 +87,8 @@ p3 <- basis_df %>%
 doc_width_cm <- 16
 doc_width_inches <- doc_width_cm *  0.3937
 
-tikz(file.path(plots_path, "basis-expansion-plot.tex"),
+tikz(file.path(plots_path, "basis-functions-plot.tex"),
      width = 1 * doc_width_inches, 
      height = 0.5 *  doc_width_inches)
 p3
 dev.off()
-
-
